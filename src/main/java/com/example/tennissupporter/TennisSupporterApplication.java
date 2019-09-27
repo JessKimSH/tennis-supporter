198068aacd65f0b9244babc0b2a35a751c64f78f
JessKim <official1126@naver.com>
2019-09-27 22:58:50 +0900

    Initial commit

---
+++ src/main/java/com/example/tennissupporter/TennisSupporterApplication.java
@@ -0,0 +1,13 @@
+package com.example.tennissupporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisSupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisSupporterApplication.class, args);
+	}
+
+}
